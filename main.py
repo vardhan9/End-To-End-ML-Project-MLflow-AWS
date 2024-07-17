e65831ad2c3692b4d71a5301bea0436cb5d7a901
Raja Marthala <raja.marthala@fliq.fi>
2024-07-17 17:42:30 +0300

    Data Transformation Done

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.mlProject import logger
 from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from src.mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+
 
 STAGE_NAME = 'Data Ingestion Stage'
 try:
@@ -18,6 +20,17 @@ STAGE_NAME = "Data Validation Stage"
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started  <<<<<")
     obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed  <<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started  <<<<<")
+    obj = DataTransformationTrainingPipeline()
     obj.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed  <<<<<")
 except Exception as e:
