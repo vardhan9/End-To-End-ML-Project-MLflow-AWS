02bc50df65d20e6294fa2839dd1ce3bde73804d6
Raja Marthala <raja.marthala@fliq.fi>
2024-07-17 16:16:50 +0300

    Data Validation Completed

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from src.mlProject import logger
 from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = 'Data Ingestion Stage'
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started  <<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed  <<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started  <<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed  <<<<<")
 except Exception as e:
     logger.exception(e)
--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,16 +1,18 @@
 from src.mlProject.constants import *
 from src.mlProject.utils.common import read_yaml, create_directories
-from src.mlProject.entity.config_entity import DataIngestionConfig
+from src.mlProject.entity.config_entity import (DataIngestionConfig, 
+                                                DataValidationConfig)
+
 class ConfigurationManager:
     def __init__(
         self,
         config_file_path = CONFIG_FILE_PATH,
         params_file_path = PARAMS_FILE_PATH,
-        schemas_file_path = SCHEMA_FILE_PATH):
+        schema_file_path = SCHEMA_FILE_PATH):
 
         self.config = read_yaml(config_file_path)
         self.params = read_yaml(params_file_path)
-        self.schemas = read_yaml(schemas_file_path)
+        self.schema = read_yaml(schema_file_path)
 
         create_directories([self.config.artifacts_root])
 
@@ -27,4 +29,18 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
-        
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
